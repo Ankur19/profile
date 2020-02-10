8262b15fbd59417baa8d4a2801bd5004ca5f3855
anksaiki <asaikia@hawk.iit.edu>
2020-02-09 20:03:03 -0600

    changed about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,22 +7,31 @@ function About(props){
     useEffect(()=>{
         if(props.scrollPosition===1){
             document.getElementById('about-1').innerHTML = "I am a Software Developer currently pursuing a Computer Science masters from Illinois Institute of Technology (3.7 gpa).";
+            document.getElementById('about-4').innerHTML = "I love coding and solving problems. Forever interested in computers and computer software. Joined IBM as a software tester in 2016 and later moved on to become a software developer. Left IBM to further work on my CS background.";
             setTimeout(()=>{
                 document.getElementById('about-2').innerHTML = "An ex-IBMer with 3+ years of experience working on Spring MVC backend and JSP/JavaScript frontend.";
+                document.getElementById('about-5').innerHTML = "Developed and tested end to end sales workflows for products like AT&T-SDWAN. Also have experience working on AGILE projects and projects implementing TDD.";
             },300);
             setTimeout(()=>{
                 document.getElementById('about-3').innerHTML = "Working on UI is my passion and getting the job done is my thing.";
+                document.getElementById('about-6').innerHTML = "Strongly motivated to learn, implement new technologies and build software. I have found that building useful software excites me.";
             }, 600)
         }
         else{
             document.getElementById('about-1').innerHTML = "";
             document.getElementById('about-2').innerHTML = "";
             document.getElementById('about-3').innerHTML = "";
+            document.getElementById('about-4').innerHTML = "";
+            document.getElementById('about-5').innerHTML = "";
+            document.getElementById('about-6').innerHTML = "";
         }
     }, [props.scrollPosition])
 
     return <div className="about-main-div">
-        <div id="about-me-text"><span id="about-1"></span><br></br><br></br> <span id="about-2"></span><br></br><br></br><span id="about-3"></span></div>
+        <div className="about-container">
+            <div id="about-me-text"><span id="about-1"></span><br></br><br></br> <span id="about-2"></span><br></br><br></br><span id="about-3"></span></div>
+            <div id="about-me-text" className="about-media-top"><span id="about-4"></span><br></br><br></br><span id="about-5"></span><br></br><br></br><span id="about-6"></span></div>
+        </div>
         <div className="about-top-details">
             <div>
                 <Hexagon image="speed_1.png"></Hexagon>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,9 +4,9 @@ import "./Contact.css";
 function Contact(props){
     return <div className="contact-main-div">
         <form>      
-            <input name="name" type="text" class="feedback-input" placeholder="Name" />   
-            <input name="email" type="text" class="feedback-input" placeholder="Email" />
-            <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
+            <input name="name" type="text" className="feedback-input" placeholder="Name" />   
+            <input name="email" type="text" className="feedback-input" placeholder="Email" />
+            <textarea name="text" className="feedback-input" placeholder="Comment"></textarea>
             <input type="submit" value="SUBMIT"/>
         </form>
     </div>
