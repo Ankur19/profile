a45a4edf09f7ef98ca0eb02ea90ae920209c56a0
Ankur Saikia <ankursaikia08@gmail.com>
2020-08-24 01:19:32 -0500

    added new links

--- src/components/MainText/MainText.js
+++ src/components/MainText/MainText.js
@@ -1,10 +1,14 @@
 import React, {useEffect} from "react";
 import "./MainText.css";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { makeStyles } from "@material-ui/core";
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
 const mainText = "< Hi, I am ";
 const mainTextSpan = "Ankur Saikia />";
 const mtSmallTxt = "I am a software developer/engineer"
-const myLoves = ["Coding", "Problem Solving", "Javascript", "Guitar"];
+const myLoves = ["Problem Solving", "C++", "Javascript", "Software", "Automation", "Guitar"];
 const viewWorkTxt = "< View my work />";
 let typeDelay = 100;
 let curLength = 0;
@@ -12,10 +16,18 @@ let curLength1 = 0;
 let curLength2 = 0;
 let myLovesIndex = 0;
 
+const useStyles = makeStyles((theme) => ({
+    externalIcons:{
+        fontSize:theme.spacing(5),
+        margin:theme.spacing(1)
+    }
+}))
+
 
 function MainText(props){
+    const classes = useStyles();
     useEffect(()=>{
-        const interval = setInterval(()=>{
+        /*const interval = setInterval(()=>{
             if( curLength < mainText.length){
                 if(mainText[curLength]===" "){
                     document.getElementById('maintext-txt1').innerHTML+= '&nbsp;';
@@ -28,7 +40,8 @@ function MainText(props){
             else{
                 clearInterval(interval);
             }
-        }, typeDelay);
+        }, typeDelay);*/
+        curLength=mainText.length;
 
         const interval1 = setInterval(()=>{
             if(curLength===mainText.length){
@@ -88,11 +101,25 @@ function MainText(props){
         return ()=> clearInterval(interval3);
     },[]);
     
+    const handleLinkedInClick = () => {
+        window.open('https://www.linkedin.com/in/ankur-saikia', "_blank") || window.location.replace('https://www.linkedin.com/in/ankur-saikia');
+    }
+    const handleGithubClick = () => {
+        window.open('https://www.github.com/ankur19', "_blank") || window.location.replace('https://www.github.com/ankur19');
+    }
+    const handleResumeDownload = () => {
+        window.open('https://www.github.com/ankur19', "_blank") || window.location.replace('https://www.github.com/ankur19');
+    }
 
     return <div className="maintext-main">
-        <p className="maintext-heading"><span id="maintext-txt1"></span><span id="maintext-txt2"></span><span id="maintext-heading-dot">&nbsp;</span></p>
+        <p className="maintext-heading"><span id="maintext-txt1">{mainText}</span><span id="maintext-txt2"></span><span id="maintext-heading-dot">&nbsp;</span></p>
         <p className="maintext-maintext"><span id="maintext-txt3"></span><span id="maintext-small-dot">&nbsp;</span></p>
         <p className="maintext-maintext"><span id="maintext-txt4"># I love:&nbsp;&nbsp;</span><span id="maintext-txt5"></span></p>
+        <div id="maintext-externals">
+            <LinkedInIcon className={classes.externalIcons} onClick={handleLinkedInClick} />
+            <GitHubIcon className={classes.externalIcons} onClick={handleGithubClick} />
+            <AssignmentIcon className={classes.externalIcons} onClick={handleResumeDownload}/>
+        </div>
         <a href="#about"><p className="maintext-footer" ><span id="maintext-link-to-next">{viewWorkTxt}</span></p></a>
     </div>
 }
